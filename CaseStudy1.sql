4b1d3d9c3742ec3289b53427db0fe8bcaaad3fd9
Dhiraj Das <dhiraj.das.05@gmail.com>
2022-02-12 18:00:07 +0530

    solutions to first 10 questions

--- CaseStudy1.sql
+++ CaseStudy1.sql
@@ -78,14 +78,147 @@ GROUP BY 1;
 
 -- 3. What was the first item from the menu purchased by each customer?
 
-
+select customer_id, array_agg(product_name) from (
+	select *, min(order_date) over (partition by customer_id) 		as first_order_date
+  from dannys_diner.sales
+)a 
+left join dannys_diner.menu b
+on a.product_id = b.product_id
+where order_date = first_order_date
+group by 1
+order by 1;
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+select customer_id, product_name, count(c.product_id)
+from dannys_diner.sales d
+inner join 
+(
+  select product_id, ranking from
+	(
+    select *, dense_rank() over (order by order_count desc) 		as ranking from 
+		(
+      select product_id, count(product_id) as order_count
+		  from dannys_diner.sales
+		  group by 1
+      )a
+    )b
+  where ranking = 1
+)c
+on c.product_id = d.product_id
+left join dannys_diner.menu e
+on e.product_id = d.product_id
+group by 1,2
+;
+
 -- 5. Which item was the most popular for each customer?
+
+select customer_id, array_agg(product_name) from 
+(
+  select *, dense_rank() over (partition by customer_id order by  item_count desc) as ranking from
+  (
+    select customer_id, product_id, count(product_id) as item_count
+	  from dannys_diner.sales
+	  group by 1,2
+  )a
+)b 
+left join dannys_diner.menu c
+on c.product_id = b.product_id
+where ranking = 1
+group by 1
+;
+
 -- 6. Which item was purchased first by the customer after they became a member?
+
+select customer_id, product_id from
+(
+  select *, dense_rank() over (partition by a.customer_id order by order_date asc) 
+  as chronological_orders from
+  (
+    select a.customer_id, a.product_id, order_date
+    from dannys_diner.sales a
+    left join dannys_diner.menu b
+    on a.product_id = b.product_id
+    inner join dannys_diner.members c
+    on a.customer_id = c.customer_id 
+    and a.order_date > c.join_date
+  )a
+)b
+where chronological_orders =1
+;
+
 -- 7. Which item was purchased just before the customer became a member?
+
+select customer_id, product_id from (
+  select *, dense_rank() over (partition by a.customer_id order by order_date desc) as chronological_orders 
+  from
+  (
+    select a.customer_id, a.product_id, order_date
+    from dannys_diner.sales a
+    left join dannys_diner.menu b
+    on a.product_id = b.product_id
+    inner join dannys_diner.members c
+    on a.customer_id = c.customer_id 
+    and a.order_date < c.join_date
+    )a
+  )b 
+  where chronological_orders =1
+;
+
 -- 8. What is the total items and amount spent for each member before they became a member?
+
+select customer_id, count(a.product_id), sum(price) from 
+(
+  select a.customer_id, a.product_id, order_date 
+  from dannys_diner.sales a
+  left join dannys_diner.menu b
+  on a.product_id = b.product_id
+  inner join dannys_diner.members c
+  on a.customer_id = c.customer_id 
+  and a.order_date < c.join_date
+)a
+left join dannys_diner.menu b
+on a.product_id = b.product_id
+group by 1
+; 
+
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+select customer_id, sum(points) from 
+(
+  select *,
+  case when product_id = 1 then 20*total_spent else 10*total_spent end as points from 
+  (
+    select customer_id, a.product_id, count(a.product_id) as qty_per_item, sum(price) as total_spent
+    from dannys_diner.sales a
+    left join dannys_diner.menu b
+    on a.product_id = b.product_id
+    group by 1,2
+  )a
+)b
+group by 1
+;
+
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 
-
+select b.customer_id, sum(points) from 
+(
+  select a.customer_id, a.product_id, 
+  case 
+    when order_date < join_date + 6 and order_date >= join_date then 20*price
+    when a.product_id = 1 then 20*price
+    when a.product_id <> 1 then 10*price 
+  end as points from 
+  (
+    select a.product_id, 
+    a.customer_id, order_date, join_date, price
+    from dannys_diner.sales a
+    left join dannys_diner.menu b
+    on a.product_id = b.product_id
+    inner join dannys_diner.members c
+    on a.customer_id = c.customer_id
+    and a.order_date <= '2021-01-31'
+  )a
+)b
+group by 1
+;
